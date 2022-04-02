5048304e4c7107975be94b159a0c3c7baa462f83
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-04-01 19:11:59 -0500

    build: add commitlint tool

---
+++ commitlint.config.js
@@ -0,0 +1,23 @@
+/* eslint-env node */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'ci',
+        'chore',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'style',
+        'release',
+      ],
+    ],
+  },
+};
---
+++ docs/improves.md
@@ -0,0 +1,7 @@
+# Next work to improve
+
+- Choose a patter like atomic design
+- Study figma to do a general design
+- Choose a tool or tools to do deploy of the app
+- To do other features of the app
+- Use commitlint to do commits with linter
