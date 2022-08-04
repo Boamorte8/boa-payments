89ebbed5e1657ea7b22f1100cc4d771f832bee84
Esteban Salazar <esteban.salazar@gorillalogic.com>
2022-08-04 16:30:16 -0500

    feat(project): add initial config for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueI18n from '@intlify/vite-plugin-vue-i18n';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command }) => ({
@@ -12,6 +13,40 @@ export default defineConfig(({ command }) => ({
     vueI18n({
       runtimeOnly: command === 'serve',
       include: path.resolve(__dirname, './src/i18n/**'),
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
+      },
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      manifest: {
+        name: 'Boa Payments',
+        short_name: 'BoaPay',
+        description: 'Boa Payments help you handle your lends and borrows',
+        theme_color: '#1d1e26',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
